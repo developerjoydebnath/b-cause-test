00b075c09003c0b1c39eb69b9ea55c3b802be7f5
Joy Debnath <developerjoydebnath@gmail.com>
2025-02-01 02:27:46 +0600

    final 3

--- src/app/(sales-reports)/page.tsx
+++ src/app/(sales-reports)/page.tsx
@@ -1,5 +1,6 @@
 import SelectLanguage from "@/components/language-select";
 import Select from "@/components/select";
+import { t } from "@/hooks/t";
 import { getDictionary } from "../actions/dictionary";
 
 export default async function Home({
@@ -14,8 +15,6 @@ export default async function Home({
     return <div>{error}</div>;
   }
 
-  const t = (text: string) => dictionary?.[text as keyof typeof dictionary];
-
   return (
     <div className="space-y-8">
       <div className="flex items-center justify-between gap-4">
@@ -23,17 +22,17 @@ export default async function Home({
           <input
             className="flex h-10 items-center justify-start rounded border px-3 py-1.5 outline-none transition-all focus:border-black"
             type="search"
-            placeholder={t("Search")}
+            placeholder={t("Search", dictionary)}
           />
           <button className="flex h-10 items-center justify-start rounded border bg-gray-100 px-3 py-1.5 outline-none transition-all hover:bg-gray-200 active:bg-gray-100">
-            {t("Clear")}
+            {t("Clear", dictionary)}
           </button>
 
-          <p>{t("Monthly, daily, by day Sales Reports")}</p>
+          <p>{t("Monthly, daily, by day Sales Reports", dictionary)}</p>
         </div>
 
         <div className="flex items-center gap-2">
-          <label>Language</label>
+          <label>{t("Language", dictionary)}</label>
           <SelectLanguage sp={sp} />
         </div>
       </div>
@@ -41,9 +40,9 @@ export default async function Home({
       <div className="flex items-center gap-4">
         <Select options={[2025, 2024]} />
         <Select options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]} />
-        <Select options={[t("Team Name") || ""]} />
-        <Select options={[t("Company Type") || ""]} />
-        <Select options={[t("New/existing") || ""]} />
+        <Select options={[t("Team Name", dictionary) || ""]} />
+        <Select options={[t("Company Type", dictionary) || ""]} />
+        <Select options={[t("New/existing", dictionary) || ""]} />
       </div>
     </div>
   );
